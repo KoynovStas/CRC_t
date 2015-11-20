0001affdc35550952914cacdb7c26ea6affd559e
KoynovStas <skojnov@yandex.ru>
2015-11-20 12:53:11 +0300

    add test for CRC_Type_Helper for uint32_t (CRC Bits 17..32)

--- crc_test.cpp
+++ crc_test.cpp
@@ -53,12 +53,45 @@ int test_crc_type_helper_uint16(struct test_info_t  *test_info)
 
 
 
+int test_crc_type_helper_uint32(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+
+    if(
+        sizeof(CRC_Type_helper< (17-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (18-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (19-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (20-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (21-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (22-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (23-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (24-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (25-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (26-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (27-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (28-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (29-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (30-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (31-1)/8 >::value_type) != sizeof(uint32_t) ||
+        sizeof(CRC_Type_helper< (32-1)/8 >::value_type) != sizeof(uint32_t)
+      )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
 
     //CRC_Type_Helper
     test_crc_type_helper_uint8,
-    test_crc_type_helper_uint16
+    test_crc_type_helper_uint16,
+    test_crc_type_helper_uint32
 };
 
 
