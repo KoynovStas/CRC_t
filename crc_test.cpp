65f0ef6219e0fef20dec82a9b0cac299a7612be7
KoynovStas <skojnov@yandex.ru>
2015-11-20 12:47:50 +0300

    add test for CRC_Type_Helper for uint8_t (CRC Bits 1..8)

--- crc_test.cpp
+++ crc_test.cpp
@@ -1,5 +1,31 @@
 #include "unit_tests.h"
+#include "universal_crc.h"
 
+
+
+
+
+int test_crc_type_helper_uint8(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+
+    if(
+        sizeof(CRC_Type_helper< (1-1)/8 >::value_type) != sizeof(uint8_t) ||
+        sizeof(CRC_Type_helper< (2-1)/8 >::value_type) != sizeof(uint8_t) ||
+        sizeof(CRC_Type_helper< (3-1)/8 >::value_type) != sizeof(uint8_t) ||
+        sizeof(CRC_Type_helper< (4-1)/8 >::value_type) != sizeof(uint8_t) ||
+        sizeof(CRC_Type_helper< (5-1)/8 >::value_type) != sizeof(uint8_t) ||
+        sizeof(CRC_Type_helper< (6-1)/8 >::value_type) != sizeof(uint8_t) ||
+        sizeof(CRC_Type_helper< (7-1)/8 >::value_type) != sizeof(uint8_t) ||
+        sizeof(CRC_Type_helper< (8-1)/8 >::value_type) != sizeof(uint8_t)
+      )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
 
 
 
@@ -7,6 +33,8 @@
 ptest_func tests[] =
 {
 
+    //CRC_Type_Helper
+    test_crc_type_helper_uint8
 };
 
 
