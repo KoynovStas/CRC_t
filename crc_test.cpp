08d5783c6fb9ae50906c565cac725d9606af0d1b
KoynovStas <skojnov@yandex.ru>
2015-11-20 15:56:12 +0300

    add test for calculate CRC-16/CDMA2000

--- crc_test.cpp
+++ crc_test.cpp
@@ -1085,6 +1085,26 @@ int test_ucrc16_4(struct test_info_t  *test_info)
 
 
 
+//width=16  poly=0xc867  init=0xffff  refin=false  refout=false  xorout=0x0000  check=0x4c06  name="CRC-16/CDMA2000"
+int test_ucrc16_5(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    uint16_t crc;
+
+    Universal_CRC<16, 0xc867, 0xffff, false, false, 0x0>  ucrc;
+
+    crc = ucrc.get_crc("123456789", 9);
+    if( crc != 0x4c06 )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
 
@@ -1160,7 +1180,8 @@ ptest_func tests[] =
     test_ucrc16,
     test_ucrc16_2,
     test_ucrc16_3,
-    test_ucrc16_4
+    test_ucrc16_4,
+    test_ucrc16_5
 };
 
 
