65317f2e985d9a21f730ae08fcb7d34dad7fed69
KoynovStas <skojnov@yandex.ru>
2017-04-05 11:06:10 +0300

    Tests: add to CRC list CRC-24/LTE-B

--- crc_test.cpp
+++ crc_test.cpp
@@ -251,6 +251,7 @@ std::vector<AbstractProxy_CRC_t *> get_crc_list()
     ADD_CRC_TO_LIST("CRC-24/FLEXRAY-B",  24, 0x5D6DCB, 0xABCDEF, false, false, 0x0,      0x1F23B8);
     ADD_CRC_TO_LIST("CRC-24/INTERLAKEN", 24, 0x328B63, 0xFFFFFF, false, false, 0xFFFFFF, 0xB4F3E6);
     ADD_CRC_TO_LIST("CRC-24/LTE-A",      24, 0x864CFB, 0x0,      false, false, 0x0,      0xCDE703);
+    ADD_CRC_TO_LIST("CRC-24/LTE-B",      24, 0x800063, 0x0,      false, false, 0x0,      0x23EF52);
 
     // CRC-31
     ADD_CRC_TO_LIST("CRC-31/PHILIPS", 31, 0x4C11DB7, 0x7FFFFFFF, false, false, 0x7FFFFFFF, 0xCE9E46C);
