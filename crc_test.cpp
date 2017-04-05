12339a71de79f78b7450c8d9278f43d4bc4fa7d1
KoynovStas <skojnov@yandex.ru>
2017-04-05 11:03:37 +0300

    Tests: add to CRC list CRC-24/INTERLAKEN

--- crc_test.cpp
+++ crc_test.cpp
@@ -245,10 +245,11 @@ std::vector<AbstractProxy_CRC_t *> get_crc_list()
     ADD_CRC_TO_LIST("CRC-16/XMODEM",      16, 0x1021, 0x0,    false, false, 0x0,    0x31C3);
 
     // CRC-24
-    ADD_CRC_TO_LIST("CRC-24",           24, 0x864CFB, 0xB704CE, false, false, 0x0, 0x21CF02);
-    ADD_CRC_TO_LIST("CRC-24/BLE",       24, 0x65B,    0x555555, true,  true,  0x0, 0xC25A56);
-    ADD_CRC_TO_LIST("CRC-24/FLEXRAY-A", 24, 0x5D6DCB, 0xFEDCBA, false, false, 0x0, 0x7979BD);
-    ADD_CRC_TO_LIST("CRC-24/FLEXRAY-B", 24, 0x5D6DCB, 0xABCDEF, false, false, 0x0, 0x1F23B8);
+    ADD_CRC_TO_LIST("CRC-24",            24, 0x864CFB, 0xB704CE, false, false, 0x0,      0x21CF02);
+    ADD_CRC_TO_LIST("CRC-24/BLE",        24, 0x65B,    0x555555, true,  true,  0x0,      0xC25A56);
+    ADD_CRC_TO_LIST("CRC-24/FLEXRAY-A",  24, 0x5D6DCB, 0xFEDCBA, false, false, 0x0,      0x7979BD);
+    ADD_CRC_TO_LIST("CRC-24/FLEXRAY-B",  24, 0x5D6DCB, 0xABCDEF, false, false, 0x0,      0x1F23B8);
+    ADD_CRC_TO_LIST("CRC-24/INTERLAKEN", 24, 0x328B63, 0xFFFFFF, false, false, 0xFFFFFF, 0xB4F3E6);
 
     // CRC-31
     ADD_CRC_TO_LIST("CRC-31/PHILIPS", 31, 0x4C11DB7, 0x7FFFFFFF, false, false, 0x7FFFFFFF, 0xCE9E46C);
